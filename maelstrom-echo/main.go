a094591489af26362a76dc4bf17ae80a34871842
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-01 21:46:26 +0000

    added initial go.mod file that contains packages and dependencies and implemented functionality to reply to the request with type changed to "echo_ok"

---
+++ maelstrom-echo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"log"
+)
+
+func main() {
+
+	n := maelstrom.NewNode()
+
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		// Declare a map with key-value pairs of type {string:any}
+		var requestBody map[string]any
+
+		//"unmarshal" or convert the message body's json AKA deserializing
+		//and return the error data structure stored in variable err
+		if err := json.Unmarshal(msg.Body, &requestBody); err != nil {
+			return err
+		}
+
+		requestBody["type"] = "echo_ok"
+
+		return n.Reply(msg, requestBody)
+
+	})
+}
